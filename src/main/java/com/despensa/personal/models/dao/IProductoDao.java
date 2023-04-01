cb9819b8cab4807385dd95737a9c7ffd1df1adf6
GS <GS@DESKTOP-IPMFBNE>
2023-04-01 02:47:05 +0200

    Primer pequeño pero gran paso.

---
+++ src/main/java/com/despensa/personal/models/dao/IProductoDao.java
@@ -0,0 +1,17 @@
+package com.despensa.personal.models.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import com.despensa.personal.models.entity.Producto;
+
+public interface IProductoDao extends JpaRepository<Producto, Long>{
+
+	@Query("SELECT p FROM Producto p INNER JOIN p.inventarios i WHERE i.almacenamiento.id = :almacenamientoId")
+	List<Producto> consultarProductosPorAlmacenamiento(@Param("almacenamientoId") Long almacenamiento_id);
+
+
+	
+}
