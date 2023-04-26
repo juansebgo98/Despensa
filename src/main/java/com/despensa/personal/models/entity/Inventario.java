d1ed48d5ea414073d89e3a16f84bf0447e2e24f5
juansebgo98 <juansebgonzalez10@gmail.com>
2023-04-27 00:00:16 +0200

    Id inventario auto generado

--- src/main/java/com/despensa/personal/models/entity/Inventario.java
+++ src/main/java/com/despensa/personal/models/entity/Inventario.java
@@ -14,6 +14,7 @@ public class Inventario implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
@@ -37,7 +38,7 @@ public class Inventario implements Serializable {
     private LocalDate fechaCaducidad;
 
 	public Inventario(Long id, Almacenamiento almacenamiento, Producto producto, Integer cantidad,
-			@NotNull BigDecimal precio, @NotNull LocalDate fechaCaducidad) {
+			BigDecimal precio, @NotNull LocalDate fechaCaducidad) {
 		super();
 		this.id = id;
 		this.almacenamiento = almacenamiento;
